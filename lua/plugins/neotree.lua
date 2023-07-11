4bd472d95b4475db3e719144f33dd7e609d995b7
KyleDeng <dlkxiaok@163.com>
2023-07-11 10:25:59 +0800

    neotree高亮

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -5,6 +5,33 @@ return {
       -- add your options that should be passed to the setup() function here
       default_component_configs = { git_status = { symbols = { unstaged = "󱈸" } } },
       window = { width = 30, mappings = { ["l"] = "open", ["h"] = "close_node" } },
+      filesystem = {
+        follow_current_file = false,
+        filtered_items = {
+          always_show = {
+            "output",
+            "apps",
+            "components",
+            "vendor",
+            "adapter",
+            "application_components",
+            "application_drivers",
+            "include",
+            "libs",
+            "build",
+          },
+        },
+        window = {
+          mappings = {
+            ["F"] = "fuzzy_finder",
+            ["/"] = "",
+          },
+        },
+      },
+      buffers = {
+        follow_current_file = false, -- This will find and focus the file in the active buffer every
+        -- time the current file is changed while the tree is open.
+      },
     },
   },
 }
