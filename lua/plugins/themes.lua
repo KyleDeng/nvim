2ed24c7cf690e695cf5f29a13dbcb50de67874a6
KyleDeng <dlkxiaok@163.com>
2024-02-20 10:45:08 +0800

    默认主题

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -2,11 +2,8 @@ return {
   -- init.lua:
   {
     "sainnhe/everforest",
-  },
-  {
-    "hardhackerlabs/theme-vim",
-    -- config = function()
-    --   vim.cmd.colorscheme("everforest")
-    -- end,
+    config = function()
+      vim.cmd.colorscheme("everforest")
+    end,
   },
 }
