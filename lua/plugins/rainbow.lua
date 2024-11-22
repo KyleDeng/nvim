5a9a9ef47cf8bb06923b082bf42d945505c71350
KyleDeng <dlkxiaok@163.com>
2024-11-22 11:38:54 +0800

    rainbow

--- lua/plugins/rainbow.lua
+++ lua/plugins/rainbow.lua
@@ -6,52 +6,5 @@ return {
   },
   {
     "nvim-treesitter/nvim-treesitter",
-    opts = {
-      highlight = { enable = true },
-      indent = { enable = true },
-      ensure_installed = {
-        "bash",
-        "c",
-        "html",
-        "javascript",
-        "json",
-        "lua",
-        "luadoc",
-        "luap",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "query",
-        "regex",
-        "tsx",
-        "typescript",
-        "vim",
-        "vimdoc",
-        "yaml",
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
-        },
-      },
-      rainbow = {
-        enable = true,
-        hlgroups = {
-          -- The colours are intentionally not in the usual order to make
-          -- the contrast between them stronger
-          "TSRainbowCyan",
-          "TSRainbowYellow",
-          "TSRainbowBlue",
-          "TSRainbowOrange",
-          "TSRainbowGreen",
-          "TSRainbowViolet",
-          "TSRainbowRed",
-        },
-      },
-    },
   },
 }
