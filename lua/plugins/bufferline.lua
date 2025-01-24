9cddf8808ebb3b7d4918c647f55317ba8d56d086
KyleDeng <55138677+KyleDeng@users.noreply.github.com>
2025-01-24 16:04:43 +0800

    Create bufferline.lua

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,11 @@
+return {
+    "akinsho/bufferline.nvim",
+    version = "*",
+    dependencies = "nvim-tree/nvim-web-devicons",
+    opts = {
+        options = {
+            -- 确保始终显示缓冲区标签
+            always_show_bufferline = true,
+        },
+    },
+}
